1df9dae66aff719564d4a658f42f0071d9a52ae6
Vishnu-Siva-B <vs21official@gmail.com>
2024-01-13 16:56:20 +0530

    final portfolio form lap

---
+++ script.js
@@ -0,0 +1,118 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+        // sticky navbar on scroll script
+        if (this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
+
+        // scroll-up button show/hide script
+        if (this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function() {
+        $('html').animate({ scrollTop: 0 });
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function() {
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function() {
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Developer", "Collage Student", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Developer", "Collage Student", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
+
+function send_message() {
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let subject = document.getElementById("subject").value;
+    let message = document.getElementById("message").value;
+
+    let templateParams = {
+        from_name: name,
+        email: email,
+        subject: subject,
+        message: message
+    };
+
+    const service_id = "service_btw9e5e";
+    const template_id = "template_6xd9qcb";
+    const publicKey = "Eos5CN8Xqe55EDPdz";
+
+    emailjs.send(service_id, template_id, templateParams, publicKey)
+        .then((response) => {
+            swal({
+                title: 'Email sent successfully!',
+                text: 'Thanks for reaching out',
+                icon: 'success'
+            });
+            document.getElementById("name").value = '';
+            document.getElementById("email").value = '';
+            document.getElementById("subject").value = '';
+            document.getElementById("message").value = '';
+        })
+        .catch((error) => {
+            var targetElement = document.getElementById("contact");
+            swal({
+                title: 'Email could not be sent!',
+                text: 'Please try again one more time',
+                icon: 'error'
+            }).then(() => {
+                targetElement.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start',
+                    inline: 'nearest'
+                });
+            });
+        });
+}
